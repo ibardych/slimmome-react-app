4559279fb8aeb4fd36d69dac9b597318719a466b
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2023-04-14 21:26:05 +0300

    modal-button

--- src/components/ModalDailyCalories/ModalDailyCalories.jsx
+++ src/components/ModalDailyCalories/ModalDailyCalories.jsx
@@ -6,12 +6,22 @@ import {
   selectNotAllowedProducts,
   selectDailyRate,
 } from 'redux/calculator/selectors';
+import { setModalOpened } from 'redux/modalOpenedSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export const ModalDailyCalories = () => {
   // const isLoading = useSelector(selectIsLoading);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const notAllowedProducts = useSelector(selectNotAllowedProducts);
   console.log(notAllowedProducts);
   const dailyRate = useSelector(selectDailyRate);
+
+  const clickHandler = () => {
+    dispatch(setModalOpened(false));
+    navigate('/diary');
+  };
 
   return (
     <Modal>
@@ -21,7 +31,7 @@ export const ModalDailyCalories = () => {
           <span className="kcal-number">{dailyRate}</span>
           <span className="kcal-text">kcal</span>
         </p>
-        {notAllowedProducts.length > 0 &&
+        {notAllowedProducts.length > 0 && (
           <div className="recomendation">
             <p className="recomendation__title">Foods you should not eat</p>
             <ol className="recomendation__list">
@@ -31,9 +41,9 @@ export const ModalDailyCalories = () => {
               <li className="recomendation__item">Smoked meats</li>
             </ol>
           </div>
-        }
+        )}
         <div className="button-container">
-          <Button className="orange" type="button">
+          <Button className="orange" type="button" onClick={clickHandler}>
             Start losing weight
           </Button>
         </div>
