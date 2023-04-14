f9054e4d9134ef74a57d350d92c4cb2f337dbec0
Olga Polishchuk <olgapolichuk@ukr.net>
2023-04-14 14:47:06 +0300

    add fix to registration

---
+++ src/redux/calculator/selectors.js
@@ -0,0 +1,5 @@
+export const selectDailyRate = state => state.calculator.dailyRate;
+
+export const selectNotAllowedProducts = state => state.calculator.notAllowedProducts;
+
+export const selectIsLoading = state => state.calculator.isLoading;
