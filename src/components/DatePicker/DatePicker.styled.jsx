7a64175c538a693102a75fd358da6922d6d40661
yanachepyk <yanapgp@gmail.com>
2023-04-18 18:50:44 +0300

    Add datepicker styles

--- src/components/DatePicker/DatePicker.styled.jsx
+++ src/components/DatePicker/DatePicker.styled.jsx
@@ -26,6 +26,43 @@ export const DatePickerContainer = styled.div`
   display: flex;
   align-items: center;
 
+  .product-days {
+    position: relative;
+
+    &::before {
+      content: '';
+      width: 7px;
+      height: 7px;
+      display: inline-block;
+      position: absolute;
+      border-radius: 50%;
+      right: 0;
+      top: 0;
+      background-color: #3d7b06;
+    }
+  }
+  .react-datepicker__day--selected {
+    background-color: ${colors.color1};
+    border-radius: 50%;
+
+    &:hover {
+       background-color: #e67b2f;
+    }
+  }
+
+  .curent-day:not(.react-datepicker__day--selected) {
+    background-color: #ffbe90;
+    border-radius: 50%;
+
+    &:hover {
+       background-color:#faad77;
+    }
+  }
+
+  .react-datepicker__day:hover {
+    border-radius: 50%;
+  }
+
   &:hover,
   &:focus {
     svg,
